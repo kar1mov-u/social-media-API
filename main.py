01479634319b5d65cbc9f1a2f66ccd4c57145313
usmon <usmonkarimov0726@gmail.com>
2024-10-17 21:38:44 +0500

    ..

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
-
+from typing import Optional
+from pydantic import BaseModel
 app = FastAPI()
+
+class Post(BaseModel):
+    title:str
+    content:str
+    published :bool = True
+    rating : Optional[int] = None
 
 @app.get('/')
 async def root():
     return {"message":"welcome niggas"}
+
+
+
+@app.get('/posts')
+async def get_posts():
+    return {"message":"All of your posts"}
+
+
+
+@app.post('/createposts')
+async def create_post(post:Post):
+    n =post.model_dump()
+    return {"data":n}
