7e751840f50861946825cbb2627055b71f1e4860
usmon <usmonkarimov0726@gmail.com>
2024-10-29 08:32:21 +0500

    Updating and Deleting with a SqlAlchmey

--- app/main.py
+++ app/main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI,HTTPException,Response,Depends
-from typing import Optional
-from pydantic import BaseModel
-from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
-from . import models
+from . import models,schemas
 from .database import engine,get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -18,10 +15,6 @@ app = FastAPI()
 
 
 
-class Post(BaseModel):
-    title:str
-    content:str
-    published :bool = True
 
 
 while True:
@@ -41,11 +34,6 @@ async def root():
     return {"message":"welcome niggas"}
 
 
-@app.get('/sqlalchemy')
-def test_posts(db:Session=Depends(get_db)):
-    
-    posts = db.query(models.Post).all()
-    return {"data":posts}
 
 
 @app.get('/posts')
@@ -59,7 +47,7 @@ async def get_posts(db:Session=Depends(get_db)):
 
 
 @app.post('/posts',status_code=201)
-async def create_post(post:Post,db:Session=Depends(get_db)):
+async def create_post(post:schemas.PostCreate,db:Session=Depends(get_db)):
     # cursor.execute(""" INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING *""",(post.title,post.content,post.published))
     # new_post = cursor.fetchone()
     # conn.commit()
@@ -71,30 +59,39 @@ async def create_post(post:Post,db:Session=Depends(get_db)):
 
 
 @app.get('/post/{id}')
-def get_post(id:int ):
-    cursor.execute("""SELECT * FROM posts WHERE id=%s """,(str(id)))
-    post = cursor.fetchone()
+def get_post(id:int,db:Session=Depends(get_db) ):
+    # cursor.execute("""SELECT * FROM posts WHERE id=%s """,(str(id)))
+    # post = cursor.fetchone()
+    post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=404, detail="Item Not Found")
     return {"message":post}
 
 
 @app.delete('/posts/{id}',status_code=204)
-def delete_post(id:int):
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""",(str(id)))
-    post = cursor.fetchone()
-    conn.commit()
+def delete_post(id:int,db:Session=Depends(get_db)):
+    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""",(str(id)))
+    # post = cursor.fetchone()
+    # conn.commit()
+    post = db.query(models.Post).filter(models.Post.id==id).first()
+
     if not post:
         raise HTTPException(status_code=404,detail="There is no such post")
+    db.delete(post)
+    db.commit()
     return Response(status_code=204)
-
+    
 
 @app.put("/posts/{id}")
-def update_post(id:int,post:Post):
-    cursor.execute("""UPDATE posts SET title =%s, content=%s, published=%s WHERE id = %s RETURNING *""", (post.title,post.content, post.published,str(id)))
-    p = cursor.fetchone()
-    conn.commit()
-    if not p:
+def update_post(id:int,post:schemas.PostCreate,db:Session=Depends(get_db) ):
+    # cursor.execute("""UPDATE posts SET title =%s, content=%s, published=%s WHERE id = %s RETURNING *""", (post.title,post.content, post.published,str(id)))
+    # p = cursor.fetchone()
+    # conn.commit()
+    post_query = db.query(models.Post).filter(models.Post.id==id)
+    old_post = post_query.first()
+    if not old_post:
         raise HTTPException(status_code=404, detail="THere is no such post")
-
-    return {"message":p}
+    post_query.update(post.model_dump(), synchronize_session = False)
+    db.commit()
+    
+    return {"message":post_query.first()}
