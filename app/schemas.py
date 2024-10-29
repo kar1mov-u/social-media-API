3b775efeaee6ab6beef41192331126c3bb92de11
usmon <usmonkarimov0726@gmail.com>
2024-10-29 23:12:56 +0500

    Created User model in DB

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr
 from datetime import datetime
+
 class PostBase(BaseModel):
     title:str
     content:str
@@ -12,5 +13,13 @@ class Post(PostBase):
     id: int
     created_at: datetime
     
-    class Config:
-        orm_mode=True
+        
+class UserCreate(BaseModel):
+    email:EmailStr
+    password:str
+
+
+class UserReturn(BaseModel):
+    id:int
+    email:str
+    created_at:datetime
